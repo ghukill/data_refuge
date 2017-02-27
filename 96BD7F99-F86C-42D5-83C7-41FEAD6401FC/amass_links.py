cb05d091f3f038b07b71caa8172863c8740d1412
Graham Hukill <ghukill@gmail.com>
2017-02-27 15:55:04 -0500

    extracting meta

--- 96BD7F99-F86C-42D5-83C7-41FEAD6401FC/amass_links.py
+++ 96BD7F99-F86C-42D5-83C7-41FEAD6401FC/amass_links.py
@@ -10,6 +10,7 @@ class Tree(object):
 
 		print "instantiating %s" % filename
 		self.soup = BeautifulSoup(open('data/trees/%s' % filename))
+		self.details_write_fail = []
 
 	def get_pubs(self):
 		table = self.soup.findAll("table", { "class" : "data_new" })
@@ -17,7 +18,7 @@ class Tree(object):
 		return table
 
 
-class Publication(object):
+class TopicPublication(object):
 
 	def __init__(self, bs4_pub):
 
@@ -39,49 +40,64 @@ class Publication(object):
 		page_html = urllib2.urlopen(self.details_url).read()
 		with open('data/pub_details/%s.html' % self.id, 'w') as f:
 			f.write(page_html)
+		print "wrote %s to disk." % self.id
+		return True
+
+class DetailsPublication(object):
+
+	def __init__(self, pub_details_filename):
+		self.filename = pub_details_filename
+		self.soup = BeautifulSoup(open('data/pub_details/%s' % self.filename))
+		self.meta_table = self.soup.findAll('table')
+		self.metadata = {
+			'Title':None
+		}
+
+
+	def parse_metadata_from_table(self):
+
+		# parse title
+		self.metadata['Title'] = self.meta_table[0].text.strip()
+
+		# extract details and append to dictionary
+		rows = self.meta_table[1].findAll('tr')
+		for row in rows:
+			cols = row.find_all('td')			
+			self.metadata[cols[0].text.strip()] = cols[1].text.strip()
 
 
 
 
-
-# as script
-if __name__ == '__main__':
-
-	# iterate through html pages
+# iterate through html pages, extract publication details
+def get_all_publication_details():
+	
 	trees = [ f for f in os.listdir('data/trees') if f.endswith('.html') ]
 	print trees
 	for tree in trees:
 
 		print "working on %s" % tree
 
-		# open as BeautifulSoup
-		# soup = BeautifulSoup(open('trees/%s' % tree))
+		# instantiate Tree
 		tree = Tree(tree)
 
-		# # get table with links
-		# table = tree.soup.findAll("table", { "class" : "data_new" })
-
-		# # pop first
-		# table.pop(0)
-
+		# iterate through pubs on page
 		for pub in tree.get_pubs():
 
-			publication = Publication(pub)
+			publication = TopicPublication(pub)
 
-			# pub_dict = {}
-			
-			# get details link
-			# a = pub.find('a')
-			# URL = a.attrs['href'].replace("http://www1.eere.energy.gov/library/asset_handler.aspx?src=","")
-			# URL = re.sub(r'&id=.*$','',URL)
-			# print URL
-			# pub_dict['URL'] = URL
+			# write pub details to disk
+			try:
+				publication.write_details_to_disk()
+			except:
+				print "########### ERROR ############"
+				tree.details_write_fail.append(pub.id)
 
-			# # get id
-			# a = pub.find('a')
 
-			# # get title
-			# pub_dict['title'] = a.text
 
-			# get description
+
+
+# as script
+# if __name__ == '__main__':
+
+
 
